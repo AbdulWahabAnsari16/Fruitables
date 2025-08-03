f20b9f9f9cfe7b28a3f0cd05c79af846a2d6c1dd
AbdulWahabAnsari16 <168890016+AbdulWahabAnsari16@users.noreply.github.com>
2025-08-03 20:48:01 +0500

    Add files via upload

---
+++ Fruitables/Fruitables/Models/Cart.cs
@@ -0,0 +1,10 @@
+﻿namespace Fruitables.Models
+{
+    public class Cart
+    {
+        public IEnumerable<addtocart> addtocarts  { get; set; }
+        public string coupons { get; set; }
+        public decimal Subtotal { get; set; }
+        public decimal Discount { get; set; }
+    }
+}
